f34c5fa6667278c9ec7c0a20c3e3c4d6e77a32fc
aldoram5 <master-sword5al@hotmail.com>
2018-01-24 20:31:38 -0600

    Moved module to a separated folder, read pickles work from renpy, added pos tagger and utilities

---
+++ Monika After Story/game/python-packages/monika_chat/monika_responses.py
@@ -0,0 +1,14 @@
+
+# Monika's predefined answers when you tell nonsense to her
+
+answers_for_gibberish =\
+    [[("Why would you say that to me?", "1c",),("That didn't make any sense,","1q")],
+                        [("What was that?","1m"),("Stop teasing me like that","1n")],
+                        [("Sorry I couldn't understand that","1o")],
+                        [("bhcsjnjdnjshn","1a"),("nuybyubefy ujjdujqwer","1r"),
+                         ("asdadsf ", "1q"),("I can say nonsense too, you know?","1a"),
+                         ("Ahaha!","1k")
+                         ]]
+
+
+monika_predef_answers = {"nonsense":answers_for_gibberish}

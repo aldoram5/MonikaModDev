9cd4dd57f8eaa818c8c79d01ec7f20c1d251250b
aldoram5 <master-sword5al@hotmail.com>
2018-02-06 01:51:38 -0600

    Added load from json methods to Conversation related classes

--- Monika After Story/game/python-packages/monika_chat/conversation.py
+++ Monika After Story/game/python-packages/monika_chat/conversation.py
@@ -1,5 +1,6 @@
 import json
 
+
 class Conversation:
 
     def __init__(self):
@@ -10,16 +11,22 @@ class Conversation:
         self.next_node = ""
         self.nodes = {}
 
-    def load_from_json(self,json_data):
+    def load_from_json(self, json_data):
         data = json.load(json_data)
+        self.load_from_array_json(data)
+
+    def load_from_array_json(self, data):
         self.trigger_verb = data["verb"]
         self.trigger_adj = data["adjective"]
         self.trigger_subject = data["subject"]
-        self.trigger_noun = data["referingNoun"]
+        self.trigger_noun = data["referringNoun"]
         self.next_node = data["nextNode"]
+        for node in data["nodes"]:
+            temp = Node()
+            temp.load_from_array_json(node)
+            self.add_node(temp)
 
-
-    def add_node(self,node):
+    def add_node(self, node):
         if not node.id or node.id is None:
             node.id = 'node'+str(len(self.nodes))
         self.nodes[node.id] = node
@@ -44,10 +51,28 @@ class Node:
         self.final_action = None
         self.options = []
 
+    def load_from_json(self, json_data):
+        data = json.load(json_data)
+        self.load_from_array_json(data)
+
+    def load_from_array_json(self, data):
+        self.id = data["id"]
+        self.input_type = data["inputType"]
+        self.next_node = data["nextNode"]
+        self.reaction = data["reaction"]
+        for option in data["options"]:
+            self.add_option(option["text"], option["nextNode"])
+        if data["initialAction"] is not None:
+            self.initial_action = Action()
+            self.initial_action.load_from_array_json(data["initialAction"])
+        if data["finalAction"] is not None:
+            self.final_action = Action()
+            self.final_action.load_from_array_json(data["finalAction"])
+
     def add_option(self,text_option, next_node_id):
         self.options.append((text_option,next_node_id))
 
-    def determine_next_node(self,selected_option):
+    def determine_next_node(self, selected_option):
         if selected_option < len(self.possible_nodes):
             return self.possible_nodes[selected_option]
         else:
@@ -67,15 +92,14 @@ class Action:
         self.key = None
         self.value = None
 
-    def load_from_json(self,json_data):
+    def load_from_json(self, json_data):
         data = json.load(json_data)
         self.load_from_array_json(data)
 
-    def load_from_array_json(self,data):
+    def load_from_array_json(self, data):
         self.type = data['type']
         self.key = data['key']
         self.value = data['value']
-
 
     def set_check_value_type(self):
         self.type = "check"

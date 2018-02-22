81b68e4b62b5d2c76439e6066b8e379ae9ca3ab7
aldoram5 <master-sword5al@hotmail.com>
2018-02-21 22:01:01 -0600

    Updated classifier to work with n-grams, it's highly customizable test required

--- Monika After Story/game/python-packages/monika_chat/dialogue_act_classifier.py
+++ Monika After Story/game/python-packages/monika_chat/dialogue_act_classifier.py
@@ -2,6 +2,7 @@ import csv
 import os
 import inspect
 import utils
+import time
 from monika_ai import MonikaAi
 from pos_tagger import PerceptronTagger
 from morphy import Morphy
@@ -15,7 +16,7 @@ class DialogueActClassifier:
             self.base_dir = os.path.dirname(inspect.getfile(self.__class__))
         self.corpus_words = {}
         self.class_words = {}
-
+        self.ngrams = 2
         self.pos_tagger = PerceptronTagger(base_dir=base_dir)
         self.morphy = Morphy(base_dir=base_dir)
 
@@ -29,6 +30,30 @@ class DialogueActClassifier:
         expanded_sentence = utils.expand_contractions(sentence.lower())
         # remove punctuation
         return utils.strip_punc(expanded_sentence)
+
+    def change_to_base(self, words):
+        """
+        change_to_base An auxiliary method that changes the input list of tuples words to base form if possible
+        :param words: list of tuples containing word and tag
+        :return: a list of words in their base form
+        """
+        base_words = []
+        for word, tag in words:
+            # use morphy to get base form and lowercase each word
+            base_word = self.morphy.morphy(word, tag)
+            base_words.append(base_word)
+        return base_words
+
+    def transform_ngrams(self, words):
+        """
+        transform_ngrams method performs a n-gram tokenization based on the self.ngrams defined, if self.ngrams is 
+        equal to 1 it return the word list as is since there's nothing to do to it.
+        Example of what this function does: for the word list : "hello", "there", "my", "love", it transforms it into a
+        list: "hello there", "there my", "my love" for ngrams = 2
+        :param words: a list of the words to tokenize
+        :return: the tokenized words
+        """
+        return words if self.ngrams == 1 else [" ".join(words[i:i+self.ngrams]) for i in range(len(words)-self.ngrams+1)]
 
     def train(self, corpus):
         """
@@ -49,20 +74,20 @@ class DialogueActClassifier:
         for data in training_data:
             # pre processing our sentence
             sentence = self.pre_process_sentence(data['sentence'])
-            print(sentence)
             # tokenize and tag each sentence into words
-            for word, tag in self.pos_tagger.tag(sentence):
-                # ignore a some things
-                # use morphy to get base form and lowercase each word
-                base_word = self.morphy.morphy(word.lower(),tag)
-                # have we not seen this word already?
-                if base_word not in self.corpus_words:
-                    self.corpus_words[base_word] = 1
+            words = self.pos_tagger.tag(sentence)
+            # use morphy to get base form and lowercase each word
+            base_words = self.change_to_base(words)
+            ngrams = self.transform_ngrams(base_words)
+            for element in ngrams:
+                # have we not seen this word combination already?
+                if element not in self.corpus_words:
+                    self.corpus_words[element] = 1
                 else:
-                    self.corpus_words[base_word] += 1
+                    self.corpus_words[element] += 1
 
                     # add the word to our words in class list
-                self.class_words[data['class']].extend([base_word])
+                self.class_words[data['class']].extend([element])
 
     # return the class with highest score for sentence
     def classify(self, sentence):
@@ -74,8 +99,7 @@ class DialogueActClassifier:
         """
         high_class = "statement"
         high_score = 0
-        print(self.corpus_words)
-        print(self.class_words)
+        trustable = True
         # loop through our classes
         for c in self.class_words.keys():
             # calculate score of sentence for each class
@@ -85,7 +109,7 @@ class DialogueActClassifier:
                 high_class = c
                 high_score = score
 
-        return high_class, high_score
+        return high_class, high_score, trustable
 
     # calculate a score for a given class taking into account word commonality
     def calculate_class_score(self,sentence, class_name, show_details=True):
@@ -93,17 +117,17 @@ class DialogueActClassifier:
         # pre processing our sentence
         sentence = self.pre_process_sentence(sentence)
         # tokenize and tag each sentence into words
-        for word, tag in self.pos_tagger.tag(sentence):
-            # use morphy to get base form and lowercase each word
-            base_word = self.morphy.morphy(word.lower(), tag)
-            # check to see if the stem of the word is in any of our classes
-            if base_word in self.class_words[class_name]:
+        words = self.pos_tagger.tag(sentence)
+        # use morphy to get base form and lowercase each word
+        base_words = self.change_to_base(words)
+        ngrams = self.transform_ngrams(base_words)
+        for element in ngrams:
+            # have we not seen this word combination already?
+            if element in self.class_words[class_name]:
                 # treat each word with relative weight
-                score += (1.0 / self.corpus_words[base_word])
-
+                score += (1.0 / self.corpus_words[element])
                 if show_details:
-                    print ("   match: %s (%s)" % (
-                        base_word, 1.0 / self.corpus_words[base_word]))
+                    print ("   match: %s (%s)" % (element, 1.0 / self.corpus_words[element]))
         return score
 
 
@@ -116,18 +140,24 @@ def command_interface():
     train = True
     s = 'hello'
     continue_chat = True
+
+    start = time.time()
     monika = DialogueActClassifier()
-    #monika.train("dialogue-act-corpus.tsv")
-    monika.train("dialogue-act-corpus-mini.tsv")
+    end = time.time()
+    print("training finished in:")
+    print(end - start)
+    monika.train("dialogue-act-corpus.tsv")
+    #monika.train("dialogue-act-corpus-mini.tsv")
     try:
         s = raw_input('> ')
     except EOFError:
         continue_chat = False
     while continue_chat:
         print(s)
-        c, s = monika.classify(s)
+        c, s, t = monika.classify(s)
         print(c)
         print(s)
+        print(t)
         if True:
             try:
                 s = raw_input('> ')
